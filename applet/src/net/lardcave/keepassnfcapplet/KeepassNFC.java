9d73705b0e5f05edf9f0a22787b1ee41e8948b6f
Avi <aki8cz@gmail.com>
2018-04-12 15:40:51 +0200

    # Memory management for scratch Area #

--- applet/src/net/lardcave/keepassnfcapplet/KeepassNFC.java
+++ applet/src/net/lardcave/keepassnfcapplet/KeepassNFC.java
@@ -297,11 +297,17 @@ public class KeepassNFC extends Applet {
 		short length = apdu.setIncomingAndReceive();
 
 		short offset = Util.getShort(buffer, ISO7816.OFFSET_CDATA);
+            if ((short)scratch_area.length > (short)(offset + length - 2)){
 		Util.arrayCopy(buffer, (short)(ISO7816.OFFSET_CDATA + 2), scratch_area, offset, (short)(length - 2));
 
 		buffer[ISO7816.OFFSET_CDATA] = RESPONSE_SUCCEEDED;
 
 		apdu.setOutgoingAndSend((short)ISO7816.OFFSET_CDATA, (short)1);
+            }
+            else {
+                buffer[ISO7816.OFFSET_CDATA] = RESPONSE_FAILED;
+		apdu.setOutgoingAndSend((short)ISO7816.OFFSET_CDATA, (short)1);
+            }
 	}
 
 	private boolean decryptWithCardKey(byte[] input, short offset, byte[] output)
